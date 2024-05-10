828a0392c0e7fbac2f6bfa3e8c12775c2b655660
Emilola Divine <emiloladivine123@gmail.com>
2024-05-10 11:44:44 +0100

    Adding the filter category in the app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import { useState } from "react";
 import ExpenseList from "./ExpenseTracker/components/ExpenseList";
+import ExpenseFilter from "./ExpenseTracker/components/ExpenseFilter";
 
 function App() {
+   const [selectedCategory, setSelectedCategory] = useState("")
    const [expenses, setExpenses] = useState([
       { id: 1, description: "bulbs", amount: 10, category: "Utilities" },
       { id: 2, description: "yam", amount: 10, category: "Groceries" },
       { id: 6, description: "rice & beans", amount: 10, category: "Utilities" },
    ]);
 
+   const visibleExpenses = selectedCategory ? expenses.filter((e) => e.category === selectedCategory) : expenses;
+
    return (
       <div className="container-md">
+         <div className="mb-3">
+            <ExpenseFilter onSelectCategory={(category) => setSelectedCategory(category)} />
+         </div>
          <ExpenseList
-            expenses={expenses}
+            expenses={visibleExpenses}
             onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
          />
       </div>
